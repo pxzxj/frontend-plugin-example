09a44a8bf944d66ef3e70e63d1f0e738686f002f
pxzxj <zhangxiongjiang@ponshine.com>
2022-01-06 17:08:12 +0800

    Initial commit

---
+++ backend/src/main/java/com/ponshine/BEApplication.java
@@ -0,0 +1,12 @@
+package com.ponshine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BEApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BEApplication.class, args);
+    }
+}
